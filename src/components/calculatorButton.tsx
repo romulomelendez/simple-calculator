a14102d733f9d93f4d017d449397d95301a3b45b
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2025-04-19 16:15:03 -0300

    feat: downloaded a react-icons and added a beautiful icon in calculator key

--- src/components/calculatorButton.tsx
+++ src/components/calculatorButton.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useDisplay } from "@/hooks"
-
+import { FaDeleteLeft } from "react-icons/fa6"
 type ButtonProps = {
   value: string | number 
 }
@@ -45,11 +45,13 @@ export const CalculatorButton = ({ value }: ButtonProps) => {
       <button
         className={value === "=" ? 
           "bg-orange-600 p-3 rounded-md text-center text-3xl text-white cursor-pointer hover:bg-orange-700"
-          : "bg-gray-800 p-3 rounded-md text-center text-3xl text-orange-600 cursor-pointer hover:bg-gray-900 hover:text-orange-700"
+          : "flex justify-center items-center bg-gray-800 p-3 rounded-md text-center text-3xl text-orange-600 cursor-pointer hover:bg-gray-900 hover:text-orange-700"
         }
         onClick={() => handleKey()}
       >
-        { value }
+        {
+          value !== "C" ? value : <FaDeleteLeft />
+        }
       </button>
     )
 }
